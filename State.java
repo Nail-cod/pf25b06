152f98587f0081254c7a6bf59a9a90d21be61b81
audreysophia <tobingaudrey28@gmail.com>
2025-06-23 11:34:26 +0700

    Merge remote-tracking branch 'origin/main'

--- State.java
+++ State.java
@@ -1,3 +1,6 @@
 public enum State {  // to save as "State.java"
     PLAYING, DRAW, CROSS_WON, NOUGHT_WON
 }
+public enum State {  // to save as "State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
