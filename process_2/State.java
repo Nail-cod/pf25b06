7822d5c34ec0fcd0cec5473ed7938a45500d3cd0
Dzak07 <ghanendradzakwan@gmail.com>
2025-06-23 11:09:45 +0700

    UPDATE README.md
    Signed-off-by: Dzak07 <ghanendradzakwan@gmail.com>

--- process_2/State.java
+++
@@ -1,7 +0,0 @@
-package process_2;
-
-
-    public enum State {  // to save as "State.java"
-        PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-    }
-
--- process_5/State.java
+++
@@ -1,5 +0,0 @@
-package process_5;
-
-public enum State {  // to save as "State.java"
-    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-}
